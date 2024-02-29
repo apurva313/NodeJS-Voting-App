b251df0ec03c1382566e8806c7b065e414cc9e83
apurva313 <apurvakumar313@gmail.com>
2024-02-29 12:13:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
+const {jwtAuthMiddleware} = require('./jwt');
+
 // Import the router files
 const userRoutes = require('./routes/userRoutes');
-// const candidateRoutes = require('./routes/candidateRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
 
 // Use the routers
 app.use('/user', userRoutes);
-// app.use('/candidate', candidateRoutes);
+app.use('/candidate', jwtAuthMiddleware, candidateRoutes);
 
 app.listen(PORT, ()=>{
     console.log('listening on port: ', PORT);
